fix flops overflow; add memops

torchanalyzer/flops.py
@@ -4,7 +4,8 @@ from torch import nn
 from torch.profiler import profile, record_function
 
 from .base import ModelAnalyzer, RecordFlowContext
-from .flops_kernel import op_map
+from .flops_kernel import flops_op_map
+from .memops_kernel import memops_op_map
 from .utils import Color, format_flops, format_percent
 
 
@@ -42,13 +43,16 @@ class ModelFlopsAnalyzer(ModelAnalyzer):
               profile(record_shapes=True, use_cuda=True) as prof):
             out = self.model(inputs)
 
-        self.flops_dict = self.summary_events(prof.events())
+        self.flops_dict = self.summary_events(prof.events(), flops_op_map)
         self.flops_all = self.flops_dict['']
+
+        self.memops_dict = self.summary_events(prof.events(), memops_op_map)
+        self.memops_all = self.memops_dict['']
 
         flow = self.add_info_to_flow(module_flow.module_record)
         return flow
 
-    def summary_events(self, events):
+    def summary_events(self, events, op_map):
         flops_dict = {}
         blocks = []
         for event in events:
@@ -79,10 +83,12 @@ class ModelFlopsAnalyzer(ModelAnalyzer):
         :return: [(name:str, info, color:str)]
         '''
         flops = self.flops_dict[name]
+        memops = self.memops_dict[name]
 
         info_list = [
             # ('Layer', f'{format_time(event.cpu_time)}, {format_percent(event.cpu_time / self.cpu_time_all)}', None, Color.CYAN),
             ('FLOPs', f'{format_flops(flops)}, {format_percent(flops / self.flops_all)}', Color.CYAN),
+            ('MemOPs', f'{format_flops(memops)}, {format_percent(memops / self.memops_all)}', Color.MAGENTA),
         ]
 
         return {'_one_': info_list}

torchanalyzer/flops_kernel.py
@@ -6,8 +6,8 @@ from torch.profiler import profile
 
 def tensor_context(func):
     def wrapper(*args, **kwargs):
-        args = [(np.array(x) if isinstance(x, list) else x) for x in args]
-        kwargs = {k: (np.array(v) if isinstance(v, list) else v) for k, v in kwargs}
+        args = [(np.array(x, dtype=np.int64) if isinstance(x, list) else x) for x in args]
+        kwargs = {k: (np.array(v, dtype=np.int64) if isinstance(v, list) else v) for k, v in kwargs}
         return func(*args, **kwargs)
 
     return wrapper
@@ -120,7 +120,7 @@ def flops_std(input_shapes, concrete_inputs):
 # -----------------------memory ops-----------------------
 
 
-op_map = {
+flops_op_map = {
     # model ops
     'aten::conv2d': flops_convnd,
     'aten::relu': flops_single_ops,
@@ -255,9 +255,9 @@ if __name__ == '__main__':
 
     device = 'cpu'
     #model = SimpleCNN().to(device)
-    model = MNISTClassifier().to(device)
+    #model = MNISTClassifier().to(device)
     #model = models.resnet18().cuda()
-    #model = timm.create_model('vit_base_patch16_224').to(device)
+    model = timm.create_model('vit_base_patch16_224').to(device)
     inputs = torch.randn(1, 3, 224, 224).to(device)
 
     with profile(record_shapes=True, use_cuda=True) as prof:
@@ -269,5 +269,5 @@ if __name__ == '__main__':
             continue
         print(
             f"{event.name} - CUDA time: {event.cuda_time}, Input shapes: {event.input_shapes}, input:{event.concrete_inputs}")
-        if event.name in op_map:
-            print(f"{event.name} - FLOPs: {op_map[event.name](event.input_shapes, event.concrete_inputs)}")
+        if event.name in flops_op_map:
+            print(f"{event.name} - FLOPs: {flops_op_map[event.name](event.input_shapes, event.concrete_inputs)}")