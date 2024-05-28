812c779fb2266810c56f47e8e2199ab5fe035ae4
IrisRainbowNeko <228218809@qq.com>
2024-05-28 23:09:23 +0800

    fix flops bugs

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt', encoding='utf8') as f:
 setuptools.setup(
     name="torch-analyzer",
     py_modules=["torch-analyzer"],
-    version="1.3",
+    version="1.4",
     author="Ziyi Dong",
     author_email="dzy7eu7d7@gmail.com",
     description="A torch model analyzer",
