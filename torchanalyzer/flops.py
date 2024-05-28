948204dff521761982d0d8b965c39ea608ebbf01
IrisRainbowNeko <228218809@qq.com>
2024-05-28 22:02:24 +0800

    count parameters

--- torchanalyzer/flops.py
+++ torchanalyzer/flops.py
@@ -45,6 +45,9 @@ class ModelFlopsAnalyzer(ModelAnalyzer):
             input_args = [input_args]
 
         self.model(*input_args, **input_kwargs)
+        self.model: nn.Module
+        self.model.named_modules()
+        self.model.parameters()
 
         with (RecordFlowContext(self.model) as module_flow, ProfContext(self.model, prefix=''),
               profile(record_shapes=True, use_cuda=True) as prof):
@@ -55,6 +58,9 @@ class ModelFlopsAnalyzer(ModelAnalyzer):
 
         self.memops_dict = self.summary_events(prof.events(), memops_op_map)
         self.memops_all = self.memops_dict['']
+
+        self.param_dict = self.count_params(self.model, {})
+        self.param_all = self.param_dict['']
 
         flow = self.add_info_to_flow(module_flow.module_record)
         return flow
@@ -75,6 +81,17 @@ class ModelFlopsAnalyzer(ModelAnalyzer):
                 blocks[-1][1] += flops
         return flops_dict
 
+    def count_params(self, module, param_dict, prefix=''):
+        param_count = 0
+        for name, child in module._modules.items():
+            if child is None:
+                continue
+            submodule_prefix = prefix + ('.' if prefix else '') + name
+            param_count += self.count_params(child, param_dict, submodule_prefix)
+        param_count += sum(p.numel() for p in module.parameters(recurse=False))
+        param_dict[prefix] = param_count
+        return param_count
+
     def add_info_to_flow(self, flow):
         new_flow = []
         for i, item in enumerate(flow):
@@ -91,11 +108,13 @@ class ModelFlopsAnalyzer(ModelAnalyzer):
         '''
         flops = self.flops_dict[name]
         memops = self.memops_dict[name]
+        params = self.param_dict[name]
 
         info_list = [
             # ('Layer', f'{format_time(event.cpu_time)}, {format_percent(event.cpu_time / self.cpu_time_all)}', None, Color.CYAN),
             ('FLOPs', f'{format_flops(flops)}, {format_percent(flops / self.flops_all)}', Color.CYAN),
             ('MemOPs', f'{format_flops(memops)}, {format_percent(memops / self.memops_all)}', Color.MAGENTA),
+            ('Parameters', f'{format_flops(params)}, {format_percent(params / self.param_all)}', Color.YELLOW),
         ]
 
         return {'_one_': info_list}
